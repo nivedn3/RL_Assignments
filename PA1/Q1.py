3446f46e909cafdb8529b3f630bca5a0c8655521
Nived <nivedn3@gmail.com>
2017-02-21 23:33:56 +0530

    PA1Q1

--- PA1/Q1.py
+++ PA1/Q1.py
@@ -2,12 +2,13 @@ import numpy as np
 from random import randint
 from numpy.random import choice
 import matplotlib.pyplot as plt
+import random
 class Bandit():
 	def __init__(self):
 		self.bandit()
 
 	def gaussian(self,offset):
-		g=np.random.normal(offset,1,1000)
+		g=np.random.normal(offset,1,10000)
 		return g
 
 	def bandit(self):
@@ -22,28 +23,34 @@ class Bandit():
 		print "tt"
 		armcount=[0 for i in range(10)]	
 		reward=[0 for i in range(1,1001)]
-		for j in range(2000):
+		for z in range(2000):
 			Q=[0 for i in range(10)]
+			#Q=np.array(Q)
 			count=[0 for i in range(10)]
-			
+			count=np.array(count)
 			Reward=[0 for i in range(1001)]
-			for i in range(1,1001):
+			for i in range(1000):
 				arm=[j for j in range(10)]
 				maxvalue=Q.index(max(Q))
 				del arm[maxvalue]
 				k=[maxvalue,arm[randint(0,8)]]
-				p=[0.9,0.1]
 
-				maxvalueE=choice(k,p=p)
+				if random.random()<0.1:
+					maxvalueE=arm[randint(0,8)]
+				else:
+					maxvalueE=maxvalue
+				#maxvalueE=choice(k,p=p)
 				count[maxvalueE]=count[maxvalueE]+1
 				armcount[maxvalueE]=armcount[maxvalueE]+1
 
-				Rt=self.a[maxvalueE][randint(0,999)]
+				Rt=self.a[maxvalueE][randint(0,9999)]
 				Q[maxvalueE]=Q[maxvalueE]+1/count[maxvalueE]*(Rt-Q[maxvalueE])
-				Reward[i]=(Rt+Reward[i-1])/i;
-			reward=[x+y for x,y in zip(reward,Reward)]
+				#Reward[i]=(Rt+(i-1)*Reward[i-1])/i;
+				reward[i]=reward[i]+Rt
+			#reward=[x+y for x,y in zip(reward,Reward)]
 			
-		reward=[i/2000 for i in reward]
+		reward=np.array(reward)
+		reward=reward/2000
 		plt.plot(reward,'r')	
 		print armcount
 		
@@ -51,11 +58,13 @@ class Bandit():
 		armcount=[0 for i in range(10)]
 		reward=[0 for i in range(1001)]
 		for j in range(2000):
-			Q=[0 for i in range(10)]	
+			Q=[0 for i in range(10)]
+			#Q=np.array(Q)	
 			count=[0 for i in range(10)]
+			count=np.array(count)
 			Reward=[0 for i in range(1001)]
 
-			for i in range(1,1001):
+			for i in range(1000):
 				#a=[j for j in range(10)]
 				maxvalueE=Q.index(max(Q))
 				#del a[maxvalue]
@@ -67,12 +76,14 @@ class Bandit():
 				count[maxvalueE]=count[maxvalueE]+1
 				armcount[maxvalueE]=armcount[maxvalueE]+1
 
-				Rt=self.a[maxvalueE][randint(0,999)]
+				Rt=self.a[maxvalueE][randint(0,9999)]
 				Q[maxvalueE]=Q[maxvalueE]+1/count[maxvalueE]*(Rt-Q[maxvalueE])
-				Reward[i]=(Rt+Reward[i-1])/i
-			reward=[x+y for x,y in zip(reward,Reward)]
+				#Reward[i]=(Rt+(i-1)*Reward[i-1])/i
+				reward[i]=reward[i]+Rt
+			#reward=[x+y for x,y in zip(reward,Reward)]
 		#plt.hist(reward,1000,normed=True)
-		reward=[i/2000 for i in reward]
+		reward=np.array(reward)
+		reward=reward/2000
 		plt.plot(reward,'g')
 		print armcount
 		plt.show()
